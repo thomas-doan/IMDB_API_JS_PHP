e43ec577b5950b73e04ab5a7f915dab43c909219
doan <thomas.doan@laplateforme.io>
2022-09-12 15:09:00 +0200

    start architecture ok, back php, front JS

---
+++ back_api/public/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMDB API</title>
+    <link rel="stylesheet" href="./css/style.css">
+
+</head>
+
+<header>
+    <nav>
+        <a href="http://"><span id="logo_name">EASY</span>Critique</a>
+        <ul class="Navigation">
+
+            <li>Accueil</li>
+            <li>Top 10</li>
+            <li>Nouveauté</li>
+            <li>Wishlist</li>
+
+        </ul>
+        <div class="hamburger">
+
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+
+        </div>
+    </nav>
+</header>
+
+<body>
+
+</body>
+
+</html>
